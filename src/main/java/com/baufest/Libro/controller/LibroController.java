a18722fe3d579e3afafa1edd0a12b4e91fdb2e5e
BAUNET\mtauro <mtauro@baufest.com>
2018-05-16 16:23:14 -0300

    merce exp2

--- src/main/java/com/baufest/Libro/controller/LibroController.java
+++ src/main/java/com/baufest/Libro/controller/LibroController.java
@@ -4,6 +4,7 @@ package com.baufest.Libro.controller;
 import com.baufest.Libro.Interfa.LibroServicio;
 import com.baufest.Libro.model.Libro;
 import com.baufest.Libro.repository.LibroRepository;
+import com.sun.tracing.dtrace.ModuleAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
 import java.awt.print.Pageable;
 import java.util.List;
-import excepciones.*;
+import com.baufest.Libro.excepciones.*;
 
 
 @RestController
@@ -31,14 +32,13 @@ public class LibroController {
 
     @RequestMapping(value = "Libro", method = RequestMethod.POST)
     public String addLibro(@ModelAttribute Libro libro){
-        String mensaje = "El libro fue creado exitosamente.";
-        try {
+        String mensaje = "Libro creado exitosamente";
+        if (libro.getNombre().isEmpty() || libro.getAutor().isEmpty()) {
+            mensaje = "No puede haber campos vacios";
+        } else {
             this.libroServicio.addLibro(libro);
-        }catch (StringVacioException sve1) {
-            mensaje = sve1.getMessage();
-        }finally {
-            return mensaje;
         }
+        return mensaje;
     }
 
   /*  @RequestMapping(value = "Libro",method = RequestMethod.DELETE)
